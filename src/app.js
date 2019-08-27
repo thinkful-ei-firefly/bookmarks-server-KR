954cc7b2387174daa7c03ebc392d97920ad7145f
Kiami Nichols <kiamimariah@yahoo.com>
2019-08-26 20:24:34 -0400

    changes

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(morgan((NODE_ENV === 'production')
   }))
 app.use(helmet())
 app.use(cors())
-app.use(validateBearerToken)
+//app.use(validateBearerToken)
 
 app.use(bookmarksRouter)
 app.get('/', (req, res) => {
